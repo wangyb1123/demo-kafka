bf37a69ed1a443e3ce6bc43e27b6b26cab401a89
wangyb1123 <wangyb1123@gmail.com>
2018-08-07 08:09:42 +0800

    Producer call back

--- src/main/java/com/demo/kafka/DefaultProducerCallback.java
+++ src/main/java/com/demo/kafka/DefaultProducerCallback.java
@@ -1,18 +1,48 @@
 package com.demo.kafka;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultProducerCallback implements Callback {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(DefaultProducerCallback.class);
+
 	@Override
 	public void onCompletion(RecordMetadata metaData, Exception exception) {
-		if(exception==null){
+		if (exception == null) {
 			logMetaData(metaData, false);
-		}else{
+		} else {
 			logMetaData(metaData, true);
 		}
-		
+
+	}
+
+	private void logMetaData(RecordMetadata metaData, boolean isError) {
+		StringBuilder sb = new StringBuilder();
+		if (isError) {
+			sb.append("Exception processing ");
+		} else {
+			sb.append("Sucessfully processed ");
+		}
+		if (metaData != null) {
+			sb.append(new ToStringBuilder(metaData,
+					ToStringStyle.SHORT_PREFIX_STYLE)
+					.append("topic", metaData.topic())
+					.append("partition", metaData.partition())
+					.append("offset", metaData.offset()));
+		} else {
+			sb.append("<No metadata supplied");
+		}
+		if (isError) {
+			LOGGER.error(sb.toString());
+		} else {
+			LOGGER.debug(sb.toString());
+		}
 	}
 
 }
